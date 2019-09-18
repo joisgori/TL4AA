7010df13ea8151ef49cda641fa9878aaf1cf3769
joisgori <37356528+joisgori@users.noreply.github.com>
2019-09-18 11:46:31 -0600

    Pido Número de casos
    Ahorita me limito a pedir el número de casos para que mi programa funcione la cantidad de veces que se necesita.

--- T4.cpp
+++ T4.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+//Variable para el número de casos
+int NCasos = 0;
+
 int main(){
-  cout << "Hola mundo";
+  cout << "Pido el número de casos ";
+  cin >> NCasos;
+
+  while(NCasos > 0){
+    cout<< "Sucede la vez número: " << NCasos << endl;
+    NCasos--;
+  }
+
+  cout << "Fin";
   return 0;
 }
